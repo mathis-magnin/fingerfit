a981d2f74a80ab6b75c05788171f4d38d88e3e5e
Élian DELMAS <elian.delmas@etu.univ-cotedazur.fr>
2024-04-07 18:29:24 +0200

    feat: avancement dans la page de creation d'une position #23

--- front-end/src/app/pages/position-creation/position-creation.component.ts
+++ front-end/src/app/pages/position-creation/position-creation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Finger, Key, Symbol } from 'src/models/quiz.model';
 
 @Component({
     selector: 'app-position-creation',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class PositionCreationComponent {
 
-    constructor() { }
+    public allKeysInRed: Key[] = [];
+
+    constructor() {
+        for (let symbol = Symbol.A; symbol <= Symbol.SPACE; symbol++) {
+            this.allKeysInRed.push({ symbol: symbol, finger: Finger.THUMB })
+        }
+    }
 
 }
