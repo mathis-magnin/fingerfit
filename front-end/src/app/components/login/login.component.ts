5705071dd18ee3189a95271dd29e58bfc887e3b3
Heschung Erwan <erwan.heschung@etu.univ-cotedazur.fr>
2024-04-10 20:11:02 +0200

    feat: functionnal login #87

--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { LoginService } from 'src/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-login',
@@ -8,10 +10,27 @@ import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
 
 export class LoginComponent implements OnInit {
 
-    @Output() public isLoggedIn: EventEmitter<boolean> = new EventEmitter<boolean>();
+    public password: string = "";
+    public passwordInput: string = "";
+    public showWarning: boolean = false;
 
-    constructor() { }
+    constructor(public loginService: LoginService, public router: Router) { 
+        loginService.password$.subscribe((password: string) => {
+            this.password = password;
+        });
+    }
     
     ngOnInit(): void { }
     
+    public onPasswordChange(event: any): void {
+        this.passwordInput = event.target.value;
+    }
+    public tryLogin(): void {
+        if (this.passwordInput === this.password) {
+            this.router.navigate(['/profiles']);
+        }
+        else {
+            this.showWarning = true;
+        }
+    }
 }
