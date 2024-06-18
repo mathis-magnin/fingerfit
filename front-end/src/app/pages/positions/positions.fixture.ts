ee4f73ae14e7e12f15ed969469c2b1a75418a735
Dorian GUIGON <dorian.guigon@etu.unice.fr>
2024-06-18 14:54:18 +0200

    test : amélioration des tests positions #177

--- front-end/src/app/pages/positions/positions.fixture.ts
+++ front-end/src/app/pages/positions/positions.fixture.ts
@@ -6,19 +6,36 @@ export class PositionsFixture extends E2EComponentFixture {
         return this.page.locator('app-button', {hasText: name});
     }
 
+    clickButton(name: string) {
+        return this.getButton(name).click();
+    }
+
     getSelector(name: string, n: number = 0) {
         return this.page.locator('select', {hasText: name}).nth(n);
     }
 
-    clickCreateButton() {
-        return this.getButton('Créer').click();
+    selectInSelector(name: string, value: string, n: number = 0) {
+        const selector = this.getSelector(name, n);
+        return selector.selectOption(value);
     }
 
     getTextInput(name: string) {
         return this.page.getByPlaceholder(name);
     }
 
+    fillInput(name: string, value: string) {
+        return this.getTextInput(name).fill(value);
+    }   
+
     getPosition() {
         return this.page.locator('app-list-item').nth(0);
+    }
+
+    countPositions() {
+        return this.page.locator('app-list-item').count();
+    }
+
+    getByText(text: string) {
+        return this.page.getByText(text);
     }
 }
