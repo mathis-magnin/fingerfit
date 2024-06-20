5689823877b5588bdb9161cef84725440b36bee1
Heschung Erwan <erwan.heschung@etu.univ-cotedazur.fr>
2024-06-20 11:42:57 +0200

    feat: ajout du volume pour le back #189

--- ops/run.sh
+++ ops/run.sh
@@ -1,13 +1,26 @@
+#!/bin/bash
 
-echo "Démarrage des services avec Docker Compose..."
-docker compose up --build -d
-
-docker compose logs back
+echo "Starting services ..."
+docker compose -f docker-compose.yml up --build -d
 
 # Affiche le statut des services
-echo "Les services en cours d'exécution:"
+echo "Services running :"
 docker compose ps
 
-echo "Pour arrêter les services, utilisez la commande suivante:"
+service_name=front
+echo "Waiting for $service_name to be healthy..."
+while :
+do
+    health_status=$(docker inspect --format='{{.State.Health.Status}}' "$(docker compose ps -q "$service_name")")
+    if [ "$health_status" == "healthy" ]; then
+        echo "Back and front are both healthy."
+        break
+    else
+        echo "Front is not healthy yet. Current status: $health_status"
+        sleep 5
+    fi
+done
+
+echo "To stop services, use command :"
 echo "docker compose down"
 
