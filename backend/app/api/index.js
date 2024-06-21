b1d5f239b524509f1286bded89b5901df8877227
Mathis MAGNIN <mathis.magnin1@etu.univ-cotedazur.fr>
2024-06-21 16:11:06 +0200

    refactor: Refonte de la page de login. On peut maintenant créer un mot de passe et le modifier. #159

--- backend/app/api/index.js
+++ backend/app/api/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express')
 const UserRouter = require('./users')
 const PositionRouter = require('./positions')
 const QuizRouter = require('./quizzes')
+const PasswordRouter = require('./passwords')
 
 const router = new Router()
 router.get('/status', (req, res) => res.status(200).json('ok'))
 router.use('/users', UserRouter)
 router.use('/positions', PositionRouter)
 router.use('/quizzes', QuizRouter)
+router.use('/passwords', PasswordRouter)
 
 module.exports = router
