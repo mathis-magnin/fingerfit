474dfc292699029abcc48f1c3c86ed6c24cb4261
ElianDELMAS <elian.delmas@univ-cotedazur.fr>
2024-06-21 17:18:16 +0200

    refactor: il est indiqué plus clairement que les positions en dessous d'un quiz sont des détails indicatifs avec un label Détails et la réduction de la taille des touches et des mains #161

--- front-end/src/app/components/quiz/quiz.component.ts
+++ front-end/src/app/components/quiz/quiz.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Quiz } from '../../../models/quiz.model';
 import { Side } from '../../../models/position.model';
-import { HandsStyle } from 'src/models/style-input.model';
+import { HandsStyle, KeyStyle } from 'src/models/style-input.model';
 
 @Component({
     selector: 'app-quiz',
@@ -12,6 +12,9 @@ export class QuizComponent {
 
     Side = Side;
     public handsStyle: HandsStyle = { width: "10vh", height: "10vh" };
+
+    public positionsHandsStyle: HandsStyle = { width: "7.5vh", height: "7.5vh" };
+    public positionsKeyStyle: KeyStyle = { fontSize: "1.25em" };
 
     @Input() quiz: Quiz | undefined = undefined;
     @Input() showDetails: boolean = false;
